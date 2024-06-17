3eb222a347e7bdf36ecc46eef66f29751efe31f9
Rishabh Gupta <rishabhtest2020@gmail.com>
2024-06-17 17:55:24 +0530

    exception handling new

--- pages/accounts_tab.py
+++ pages/accounts_tab.py
@@ -59,6 +59,7 @@ class Accounts:
 
     def select_rm_norms_field(self, browser, rm_type=2):
         do_click(browser, rm_norms)
+        loader_should_be_invisile(browser, 2)
         values = get_list_of_elems_text(browser, rm_norms_options[0], rm_norms_options[1])
         assert values == rm_type_list
         select_rm = rm_norms_options[1] + f'[{rm_type}]'
@@ -70,6 +71,7 @@ class Accounts:
     def select_business_nature_field(self, browser, bn_type=2):
         should_be_visible(browser, business_info_h3, 'business_info_h3')
         do_click(browser, business_nature)
+        loader_should_be_invisile(browser, 2)
         values = get_list_of_elems_text(browser, business_nature_option_txt[0], business_nature_option_txt[1])
         assert values[3:] == business_nature_list
         select_bn = business_nature_option[1] + f'[{bn_type}]'
@@ -81,6 +83,7 @@ class Accounts:
 
     def select_business_domain_field(self, browser, bd_type=2):
         do_click(browser, business_domain)
+        loader_should_be_invisile(browser, 2)
         values = get_list_of_elems_text(browser, business_domain_option_txt[0], business_domain_option_txt[1])
         assert values[3:] == business_domain_list
         select_bd = business_domain_option[1] + f'[{bd_type}]'
@@ -92,6 +95,7 @@ class Accounts:
 
     def select_business_segment_field(self, browser, bs_type=2):
         do_click(browser, business_segment)
+        loader_should_be_invisile(browser, 2)
         values = get_list_of_elems_text(browser, business_segment_option_txt[0], business_segment_option_txt[1])
         assert values[3:] == business_segment_list
         select_bs = business_segment_option[1] + f'[{bs_type}]'
@@ -102,6 +106,7 @@ class Accounts:
         assert self.business_segment == business_segment_list[bs_type - 1]
 
     def select_payment_method_field(self, browser, pm_type=2):
+        loader_should_be_invisile(browser, 2)
         scroll_into_the_view(browser, payment_details_h3[0], payment_details_h3[1])
         should_be_visible(browser, payment_details_h3, 'payment_details_h3')
         do_click(browser, payment_method)
@@ -116,6 +121,7 @@ class Accounts:
 
     def select_payment_term_field(self, browser, pt_type=3):
         do_click(browser, payment_term)
+        loader_should_be_invisile(browser, 2)
         values = get_list_of_elems_text(browser, payment_term_options[0], payment_term_options[1])
         assert values == payment_term_list
         select_pt = payment_term_options[1] + f'[{pt_type}]'
@@ -135,6 +141,7 @@ class Accounts:
             do_send_keys(browser, bill_loc, data)
 
     def select_country_field(self, browser, country='India'):
+        loader_should_be_invisile(browser, 2)
         do_click(browser, billing_country)
         values = get_list_of_elems_text(browser, billing_country_options[0], billing_country_options[1])
         assert values == billing_countries_list
@@ -143,11 +150,14 @@ class Accounts:
         scroll_into_the_view(browser, select_cou_loc[0], select_cou_loc[1])
         self.country = get_element_text(browser, select_cou_loc)
         do_click(browser, select_cou_loc)
+        loader_should_be_invisile(browser, 2)
         do_click(browser, Billing_Address1_h3)
         sleep(0.5)
 
     def select_state_field(self, browser, state='Uttar Pradesh'):
         do_click(browser, billing_state)
+        loader_should_be_invisile(browser, 2)
+        loader_should_be_invisile(browser, 2)
         values = get_list_of_elems_text(browser, billing_state_options[0], billing_state_options[1])
         assert values == billing_india_states_list
         select_cou = billing_state_select[1].replace('state_name', state)
@@ -160,6 +170,8 @@ class Accounts:
 
     def select_city_field(self, browser, city='Ghaziabad'):
         do_click(browser, billing_city)
+        loader_should_be_invisile(browser, 2)
+        loader_should_be_invisile(browser, 2)
         values = get_list_of_elems_text(browser, billing_city_options[0], billing_city_options[1])
         assert values == billing_uttar_pradesh_cities_list
         select_cou = billing_city_select[1].replace('city_name', city)
